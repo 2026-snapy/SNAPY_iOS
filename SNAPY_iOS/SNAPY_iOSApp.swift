0a99a4a7566cde52bbba26f3dfd236ddce1792ac
kimeunchan <kec4489@naver.com>
2026-03-17 01:30:08 +0900

    feat: 프로젝트 생성

---
+++ SNAPY_iOS/SNAPY_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNAPY_iOSApp.swift
+//  SNAPY_iOS
+//
+//  Created by 김은찬 on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SNAPY_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
